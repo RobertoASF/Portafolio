126dd7685e601c04dac53893761090baaad2929d
IgriegaL <yehudilara@hotmail.com>
2023-04-09 01:33:04 -0400

    Se agrega cambio en ALLOWED_HOSTS = ['*']

--- tindplace/settings.py
+++ tindplace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-(^5ycufja7wu^3@eyvl5h8cuo$=#&!s#c$kjf00yzojuw4jnw&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
